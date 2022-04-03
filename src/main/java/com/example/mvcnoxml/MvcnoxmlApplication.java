6b78773f9b9bb605f972dbb31d370a93d2dc0519
itay1551 <itaykatavvv@gmail.com>
2022-04-03 21:01:55 +0300

    basic mvc web app

---
+++ src/main/java/com/example/mvcnoxml/MvcnoxmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcnoxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcnoxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcnoxmlApplication.class, args);
+	}
+
+}
